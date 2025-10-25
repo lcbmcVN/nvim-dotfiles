67b7d611eefb060f2ae1a820b44182370cb9b262
Bang Lee <60475468+lcbmcVN@users.noreply.github.com>
2025-10-25 19:54:12 +0700

    Delete nvimconf directory

--- nvimconf/after/plugin/telescope.lua
+++
@@ -1,7 +0,0 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<C-f>', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
--- vim.keymap.set('n', '<C-f>', builtin.git_files, { desc = 'Telescope find files' })
-
